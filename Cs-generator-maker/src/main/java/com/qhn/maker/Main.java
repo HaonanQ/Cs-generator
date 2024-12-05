9cba8bee19ce477db0830553358ae11026b78a95
北鱼 <13287774+zhuifengbeiyu@user.noreply.gitee.com>
2024-12-05 11:40:12 +0800

    实现了代码生成器制作工具的开发

--- Cs-generator-maker/src/main/java/com/qhn/maker/Main.java
+++ Cs-generator-maker/src/main/java/com/qhn/maker/Main.java
@@ -1,11 +1,11 @@
 package com.qhn.maker;
 
-import com.qhn.maker.cli.CommandExecutor;
+//import com.qhn.maker.CommandExecutor;
 
 public class Main {
     public static void main(String[] args) {
 //        args = new String[] {"config"};
-        CommandExecutor commandExecutor = new CommandExecutor();
-        commandExecutor.execute(args);
+//        CommandExecutor commandExecutor = new CommandExecutor();
+//        commandExecutor.execute(args);
     }
 }
